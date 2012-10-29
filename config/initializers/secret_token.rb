9ca8d1315bad8cc259200ee8f8e311be1dafe903
liplw016 <liplw016@liplw016-Vostro-1550.(none)>
2012-10-29 14:29:12 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorhub::Application.config.secret_token = '79bdd262dcc91977c2b09fd1e3666ac206c4287ef6c4eb54a6dcba8af170a3506d9207ce13ec96e0f2bd1172e9fc0873f730850aafbe4758d0d1bac7036cb200'
