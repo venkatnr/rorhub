fd5b0fe33cb52a861a458aaa47882b19b41c5123
lakshmi <lakshmi@lakshmi-Lenovo-G570.(none)>
2012-11-03 14:35:03 +0530

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,6 @@ class User
   # field :authentication_token, :type => String
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
   data = access_token.info
-  raise data.inspect
 	  if user = User.where(:email => data["email"]).first
 		user
 	  else
